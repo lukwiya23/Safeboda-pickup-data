8d627a30ecb0ac3b3b5b34b6543e5f0ed9198492
lukwiya23 <luk23bonnie8@gmail.com>
2020-05-27 16:05:36 +0300

    setting up caching mechanism

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import time
 
 st.title('Safe boda pickups in Kampala')
 
+DATE_COLUMN = 'date/time'
+
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache
