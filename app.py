feaf37cf7579d8d8cb55b3deae7e217bc5051acf
lukwiya23 <luk23bonnie8@gmail.com>
2020-05-30 16:21:51 +0300

    plotting histogram of acquired filtered data

--- app.py
+++ app.py
@@ -28,3 +28,4 @@ if st.checkbox('show raw data'):
 
 st.subheader('Number of pickups by the hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
