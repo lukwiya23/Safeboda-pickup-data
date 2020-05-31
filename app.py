9a0ccc7b0eaca721581541e05419abfe2aee99b0
lukwiya23 <luk23bonnie8@gmail.com>
2020-05-31 16:24:56 +0300

    filtering out most active hours

--- app.py
+++ app.py
@@ -31,3 +31,4 @@ hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 
 hour_to_filter = st.slider('hour',0,23,17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
