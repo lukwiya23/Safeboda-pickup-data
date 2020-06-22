1c40fb63901941c56f4dc3151138fbe840dd14ea
lukwiya23 <luk23bonnie8@gmail.com>
2020-06-22 15:58:20 +0300

    setting up

--- ml.py
+++ ml.py
@@ -1,27 +1,32 @@
-import yfinance as yf
 import streamlit as st
 import pandas as pd
+import numpy as np
+import time
 
-st.write("""
-# Simple Stock Price App
+st.title('Safe boda pickups in Kampala')
 
-shown below are the stock closing price and volume of Google
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-""")
-
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-
-#get data of the ticker
-tickerData = yf.Ticker(tickerSymbol)
-
-#get the historical prices of the year
-
-tickerDf = tickerData.history(period='1d', start='2015-5-31',end='2020-5-31')
-
-#Open  High  Low Close  Volume  Dividends  Stock splits
-
-st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
